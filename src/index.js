345a308a16ef872cfa57ed489a5100c5328e5a51
minhquang111 <minhquang2532000@gmail.com>
2022-05-13 13:51:55 +0700

    update-project

--- src/index.js
+++ src/index.js
@@ -2,18 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import store from "./store";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import store from "./slices/store";
 
 // style
-import "./styles/scss/style.scss";
+import "./styles/scss/index.scss";
 import "antd/dist/antd.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
---
+++ src/routers.js
@@ -0,0 +1,5 @@
+export const routes = [
+
+];
+
+// config router v5: https://v5.reactrouter.com/web/example/route-config
